4682683aa2c9dd3c965327808d60634147bee588
Amogh Kini <amoghkini@gmail.com>
2023-07-29 00:47:56 +0530

    dark mode issue fixed

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,23 +1,42 @@
-const darkModeSwitch = document.getElementById('darkModeSwitch');
-const navbar = document.getElementById('navbar');
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the dark mode switch and store it in a variable
+    const darkModeSwitch = document.getElementById('darkModeSwitch');
+    const navbar = document.getElementById('navbar');
 
-// Function to toggle dark mode
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    navbar.classList.toggle('navbar-dark', document.body.classList.contains('dark-mode'));
-    navbar.classList.toggle('navbar-light', !document.body.classList.contains('dark-mode'));
-
-    // Save the user's preference to local storage
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('darkMode', 'enabled');
-    } else {
-        localStorage.setItem('darkMode', 'disabled');
+    // Function to set the theme based on the current dark mode setting
+    function setTheme() {
+        if (document.body.classList.contains('dark-mode')) {
+            // Set dark mode theme
+            navbar.classList.remove('navbar-light');
+            navbar.classList.add('navbar-dark');
+            darkModeSwitch.checked = true; // Dark mode, so switch is on
+        } else {
+            // Set light mode theme
+            navbar.classList.remove('navbar-dark');
+            navbar.classList.add('navbar-light');
+            darkModeSwitch.checked = false; // Light mode, so switch is off
+        }
     }
-}
 
-// Check the user's preference from local storage, if available
-if (localStorage.getItem('darkMode') === 'enabled') {
-    toggleDarkMode();
-}
+    // Function to toggle dark mode on or off
+    function toggleDarkMode() {
+        document.body.classList.toggle('dark-mode');
+        setTheme();
 
-darkModeSwitch.addEventListener('click', toggleDarkMode);
+        // Save user's preference to local storage
+        localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
+    }
+
+    // Check if the user has previously selected dark mode
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+    // Set initial dark mode based on user's previous selection
+    if (isDarkMode) {
+        toggleDarkMode();
+    } else {
+        setTheme(); // Set theme based on current dark mode setting
+    }
+
+    // Add event listener to the dark mode switch
+    darkModeSwitch.addEventListener('change', toggleDarkMode);
+});
