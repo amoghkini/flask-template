9b4cf41b60ffb657e043b8840fc7268decff1640
Amogh Kini <amoghkini@gmail.com>
2023-09-17 21:16:51 +0530

    Enhanced dropdown.js to handle rendering of dropdown values

--- src/static/js/dropdown.js
+++ src/static/js/dropdown.js
@@ -2,24 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get a reference to the dropdown element
     const dropdown = document.getElementById('dropdown');
 
-    // Get the page name from the data attribute
-    const page = dropdown.getAttribute('data-page');
+    // Check if the 'dropdown' element exists in the DOM
+    if (dropdown) {
+        // Get the page name from the data attribute
+        const page = dropdown.getAttribute('data-page');
 
-    // Fetch data from your Flask route with the page parameter
-    fetch(`/get_dropdown_data/${page}`)
-        .then(response => response.json())
-        .then(data => {
-            // Iterate over the data and create <option> elements
-            data.forEach(optionText => {
-                const option = document.createElement('option');
-                option.value = optionText;
-                option.textContent = optionText;
-                dropdown.appendChild(option);
+        // Fetch data from your Flask route with the page parameter
+        fetch(`/get_dropdown_data/${page}`)
+            .then(response => response.json())
+            .then(data => {
+                // Iterate over the data and create <option> elements
+                data.forEach(optionText => {
+                    const option = document.createElement('option');
+                    option.value = optionText;
+                    option.textContent = optionText;
+                    dropdown.appendChild(option);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
             });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+    }
 });
-
-
